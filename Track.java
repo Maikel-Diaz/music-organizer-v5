aa22fad79231bfb455a40c1c7a2114d88649e668
Maikel <maikel_20@hotmail.es>
2018-01-23 21:26:38 +0100

    Se ha añadido un contador y dos metodos para ponerlo a cero e incrementarlo

--- Track.java
+++ Track.java
@@ -14,7 +14,7 @@ public class Track
     // Where the track is stored.
     private String filename;
     
-    
+    private int playCount;
     /**
      * Constructor for objects of class Track.
      * @param artist The track's artist.
@@ -24,6 +24,7 @@ public class Track
     public Track(String artist, String title, String filename)
     {
         setDetails(artist, title, filename);
+        playCount = 0;
     }
     
     /**
@@ -35,6 +36,7 @@ public class Track
     public Track(String filename)
     {
         setDetails("unknown", "unknown", filename);
+        playCount = 0;
     }
     
     /**
@@ -86,4 +88,13 @@ public class Track
         this.filename = filename;
     }
     
+    public void resetearPlayCount()
+    {
+        playCount = 0;
+    }
+    
+    public void incrementarPlayCount()
+    {
+        playCount = playCount + 1;
+    }
 }
